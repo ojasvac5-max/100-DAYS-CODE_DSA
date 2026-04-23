f48d255a8be6bd10ae4c5e227d0eb3428d0e4cc9
ojasvac5-max <ojasvac5@gmail.com>
2026-04-23 22:00:45 +0530

    DSA_66

---
+++ DSA_100_66.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+#define SIZE 100
+
+int adj[SIZE][SIZE];
+int visited[SIZE];
+int recStack[SIZE];
+
+int dfs(int node, int n) {
+    visited[node] = 1;
+    recStack[node] = 1;
+
+    for(int i = 0; i < n; i++) {
+        if(adj[node][i]) {
+            
+            if(!visited[i]) {
+                if(dfs(i, n)) return 1;
+            }
+            else if(recStack[i]) {
+                return 1; 
+            }
+        }
+    }
+
+    recStack[node] = 0; 
+    return 0;
+}
+
+int hasCycle(int n) {
+    for(int i = 0; i < n; i++) {
+        if(!visited[i]) {
+            if(dfs(i, n)) return 1;
+        }
+    }
+    return 0;
+}
+
+int main() {
+    int n, m;
+    printf("Enter number of nodes and edges: ");
+    scanf("%d %d", &n, &m);
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            adj[i][j] = 0;
+        }
+        visited[i] = 0;
+        recStack[i] = 0;
+    }
+
+    printf("Enter directed edges (u v):\n");
+    for(int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        adj[u][v] = 1; 
+    }
+
+    if(hasCycle(n)) {
+        printf("YES\n");
+    } else {
+        printf("NO\n");
+    }
+
+    return 0;
+}
