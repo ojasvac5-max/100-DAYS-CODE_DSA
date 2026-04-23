9abe59dbda69e997c9a7dd6c93d1af959d421a75
ojasvac5-max <ojasvac5@gmail.com>
2026-04-23 20:27:35 +0530

    DSA_56

---
+++ DSA_100_56.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+struct TreeNode* newNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = node->right = NULL;
+    return node;
+}
+int isMirror(struct TreeNode* t1, struct TreeNode* t2) {
+    if (t1 == NULL && t2 == NULL) return 1;
+    if (t1 == NULL || t2 == NULL) return 0;
+
+    return (t1->val == t2->val) &&
+           isMirror(t1->left, t2->right) &&
+           isMirror(t1->right, t2->left);
+}
+int isSymmetric(struct TreeNode* root) {
+    if (root == NULL) return 1;
+    return isMirror(root->left, root->right);
+}
